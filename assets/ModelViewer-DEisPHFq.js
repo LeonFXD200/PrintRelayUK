9dffab1c6aa1fb5d3b75f3dfd0db5630ace49d43
LeonFXD200 <l3onfxdupree@gmail.com>
2026-06-25 01:43:17 +0100

    Updates

--- assets/ModelViewer-DEisPHFq.js
+++ assets/ModelViewer-DEisPHFq.js
@@ -1,4 +1,4 @@
-import{r as na,j as ci,B as Zo}from"./index-oHxPW3xf.js";/**
+import{r as na,j as ci,B as Zo}from"./index-oOMMFjfY.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
