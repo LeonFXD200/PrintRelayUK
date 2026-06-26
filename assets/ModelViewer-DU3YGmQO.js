61413eb671a506790dc586338d7f0bcaa930c4d6
LeonFXD200 <l3onfxdupree@gmail.com>
2026-06-26 18:36:45 +0100

    Updates

--- assets/ModelViewer-DU3YGmQO.js
+++ assets/ModelViewer-DU3YGmQO.js
@@ -1,4 +1,4 @@
-import{r as na,j as ci,B as Zo}from"./index-oOMMFjfY.js";/**
+import{r as na,j as ci,B as Zo}from"./index-mJQ98gvg.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
